7e108b9eccbc660aecee2f8a25a9c7c0d4927490
IGOR CHIGUR <chigur.ig@gmail.com>
2019-01-04 21:14:53 +0200

    commit 5

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http=require('http');
+const PORT = process.env.PORT || 5000;
+
 http.createServer((req,res)=>{
     console.log(req.url);
     console.log(req.method);
     console.log(req.headers);
     res.writeHead(200,{"Content-Type":"text/html"});
     res.end('<!doctype><html><head><meta charset="utf-8"><title>Osnovy Node.JS</title></head><body><h1>Osnovy node js</h1></body></html>');
-}).listen(3000,()=>console.log("server rabotaet"));
+}).listen(PORT,()=>console.log("server rabotaet"));
