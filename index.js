a6b9d25965dbdd0f3559f03558924e8db9edad4b
IGOR CHIGUR <chigur.ig@gmail.com>
2019-01-04 21:59:15 +0200

    commit 6

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ http.createServer((req,res)=>{
     console.log(req.url);
     console.log(req.method);
     console.log(req.headers);
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
     res.writeHead(200,{"Content-Type":"text/html"});
-    res.end('<!doctype><html><head><meta charset="utf-8"><title>Osnovy Node.JS</title></head><body><h1>Osnovy node js</h1></body></html>');
+    // res.end('<!doctype><html><head><meta charset="utf-8"><title>Osnovy Node.JS</title></head><body><h1>Osnovy node js</h1></body></html>');
+    res.end(txt);
+
 }).listen(PORT,()=>console.log("server rabotaet"));
